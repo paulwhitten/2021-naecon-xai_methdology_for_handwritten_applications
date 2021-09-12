ffb9d9bbdf33cb3ce4e157e7237c5548b707f19e
pcw <pcw@case.edu>
2021-09-11 22:11:59 -0400

    add images

---
+++ digit-images/index.js
@@ -0,0 +1,29 @@
+const { Image } = require('image-js');
+const fs = require('fs')
+
+function test() {
+    fs.readdir('./', (err, files) => {
+        if (err) {
+            console.log('Error reading folder', err);
+            process.exit(1);
+        }
+        for (let file of files) {
+            if (file.endsWith('.png')) {
+                console.log(file);
+                Image.load(file).then((image) => {
+                    let max = image.size;
+                    for (let i = 0; i < max; i++) {
+                        let newVal = 255 - image.getPixel(i)[0];
+                        image.setPixel(i, [newVal]);
+                    }
+                    image.save(file);
+                })
+                .catch((loadErr) => {
+                    console.log('Error loading', loadErr);
+                })
+            }
+        }
+    })
+}
+
+test();
